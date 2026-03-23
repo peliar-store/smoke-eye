fd4800254b534e2106c118f3d0d8041cfb69ced8
peliar-store <tetrawallet@outlook.com>
2026-03-23 10:40:43 +0200

    skeleton added

---
+++ preload.js
@@ -0,0 +1,15 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  startServer: (port) => ipcRenderer.invoke('start-server', port),
+  connect: (host, port) => ipcRenderer.invoke('connect', { host, port }),
+  disconnect: () => ipcRenderer.invoke('disconnect'),
+  sendMessage: (payload) => ipcRenderer.invoke('send-message', payload),
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  setSettings: (s) => ipcRenderer.invoke('set-settings', s),
+  suspendHotkeys: () => ipcRenderer.invoke('suspend-hotkeys'),
+  resumeHotkeys: () => ipcRenderer.invoke('resume-hotkeys'),
+  onChatMessage: (cb) => ipcRenderer.on('chat-message', (_e, msg) => cb(msg)),
+  onPeerStatus: (cb) => ipcRenderer.on('peer-status', (_e, s) => cb(s)),
+  onHotkeyAction: (cb) => ipcRenderer.on('hotkey-action', (_e, a) => cb(a))
+});
